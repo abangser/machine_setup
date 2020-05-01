c9a6940efc60d3743a9560879d9744fd757a17da
Abby Bangser <bangser.a@gmail.com>
2020-05-01 23:24:49 +0100

    add bluejeans

--- configuration.nix
+++ configuration.nix
@@ -66,14 +66,19 @@ in
     };
 
     environment.systemPackages = with pkgs; [
+      bluejeans-gui
       lsof
       vim
       wget
     ];
 
-    nixpkgs.config.packageOverrides = pkgs: {
-      nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-        inherit pkgs;
+    nixpkgs.config = {
+      allowUnfree = true;
+
+      packageOverrides = pkgs: {
+        nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+          inherit pkgs;
+        };
       };
     };
 
